11a2d6c64ec732c2a1c913cee3352cd909650f8d
wubinary <wubinray@gmail.com>
2021-04-07 06:34:11 +0800

    hw3+advanced

--- hw3/own/src/containers/TodoApp.js
+++ hw3/own/src/containers/TodoApp.js
@@ -1,148 +1,286 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
-import Input from "../components/Input";
 
-// TodoApp
-
+// Todo App
 class TodoApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {todoTotal:0, Total:0, completeTotal:0, showMode:"all", clear:0};
+    }
+
+    setTotal = (total_active, total)=>{
+        this.setState((state)=>({todoTotal: total_active, Total: total}));
+    }
+
+    getTodoTotal = ()=>{
+        return this.state.todoTotal;
+    }
+
+    getTotal = ()=>{
+        return this.state.Total;
+    }
+
+    setShowMode = (showMode)=>{
+        this.setState((state)=>({showMode:showMode}));
+    }
+
+    setCompleteTotal = (completeTotal)=>{
+        this.setState((state)=>({completeTotal:completeTotal}));
+    }
+
+    getCompleteTotal = ()=>{
+        return this.state.completeTotal;
+    }
+
+    setClear = (clear)=>{
+        this.setState((state)=>({clear: clear}));
+    }
+
+    getClear = ()=>{
+        return this.state.clear;
+    }
+
     render() {
         return (
-            <div className="todo-app__root">
+            <div className="todo-app__root" id="root">
                 <Header text="todos" />
-                <Main />
-                <TodoControl />
+                <Section setTotal={this.setTotal} 
+                         setCompleteTotal={this.setCompleteTotal} 
+                         showMode={this.state.showMode} 
+                         setClear={this.setClear} 
+                         getClear={this.getClear} />
+                <Footer getTodoTotal={this.getTodoTotal} 
+                        getTotal={this.getTotal} 
+                        getCompleteTotal={this.getCompleteTotal}
+                        setShowMode={this.setShowMode}
+                        setClear={this.setClear} />
             </div>
         );
     }
 }
 
-// Main
-var todoList = [];
-var showlen = 0;
+// Footer
+class Footer extends React.Component {
+    show = ()=>{
+        if (this.props.getTotal() === 0) 
+            return {'display': 'none'};
+        return {};
+    }
 
-class todoItem {
-    constructor(id, text, status=0) {
+    completedButtonShow = ()=>{
+        if (this.props.getCompleteTotal() > 0)
+            return {};
+        return {'display': 'none'};
+    }
+
+    render() {
+        return (
+            <footer className="todo-app__footer" id="todo-footer" style={this.show()}>
+                <div className="todo-app__total">{this.props.getTodoTotal()} left</div>
+                <ul className="todo-app__view-buttons">
+                    <button onClick={()=>{this.props.setShowMode("all")}}>All</button>
+                    <button onClick={()=>{this.props.setShowMode("active")}}>Active</button>
+                    <button onClick={()=>{this.props.setShowMode("completed")}}>Completed</button>
+                </ul>
+                <div className="todo-app__clean">
+                    <button style={this.completedButtonShow()} onClick={()=>{this.props.setClear(1)}} >Clear Completed</button>
+                </div>
+            </footer>
+        );
+    }
+}
+
+// Todo Datastructure
+class Todo {
+    constructor(id, text, status=0, checkStyle={}, checkLabelStyle={}) {
         this.id = id;
         this.text = text;
         this.status = status;
+        this.checkStyle = checkStyle; //{text_decoration: "line-through", opacity: 0.5};
+        this.checkLabelStyle = checkLabelStyle;
     }
 }
 
-class TodoItem extends React.Component {
+// Section 
+class Section extends Component {
     constructor(props) {
         super(props);
-        this.state = {status: 0};
+        this.state = {id_count:0, list:[]};
     }
 
-    check_onClick = () => {
-        this.props.todoItem.status = this.props.todoItem.status==1 ? 0:1 ;
-        this.setState({status: this.state.status++});
-        this.forceUpdate();
-    }
-
-    render() {
-        var status = this.props.todoItem.status;
-        var checkbox_style={}, item_detail_style={};
-        if (status) {
-            checkbox_style = {background: "green"};
-            item_detail_style = {text_decoration: "line-through", opacity: 0.5};
+    clearCompleted = ()=>{
+        if (this.props.getClear() === 1) {
+            var list = [];
+            for (var i=0; i<this.state.list.length; i++)
+                if ( this.state.list[i].status === 0 )
+                    list.push(this.state.list[i]);
+            this.setState((state)=>({list: list}));
+            this.props.setClear(0);
         }
-        return (
-            <li key={this.props.todoItem.id.toString()} className="todo-app__item">
-                <div className="todo-app__checkbox" onClick={this.check_onClick} style={checkbox_style}></div>
-                <h1 className="todo-app__item-detail" style={item_detail_style}>
-                     {this.props.todoItem.text}
-                </h1>
-                <img className="todo-app__item-x" src="./img/x.png" />
-            </li>
-        );
-    }
-}
-
-class TodoList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {list: props.todoList};
     }
 
-    componentDidMount = ()=>{
-        setInterval(()=>{this.setState({list: this.props.todoList});}, 500);
-    }
-
-    get_rows = ()=>{
-        var rows = [];
-        for (var i=this.state.list.length-1; i>=0; i--) {
-            var item = this.state.list[i];
-            rows.push(<TodoItem todoItem={item} />);
+    updateTotal = ()=> {
+        var total = 0, total_active = 0;
+        for (var i=0; i<this.state.list.length; i++) {
+            if ( this.state.list[i].status === 0 )
+                total_active ++;
+            total ++;
         }
-        showlen = rows.length;
-        return rows;
+        this.props.setTotal(total_active, total);
     }
 
-    render() {
-        console.log(this.props.todoList);
-        return (
-            <ul className="todo-app__list" id="todo-list">{this.get_rows()}</ul>
-        );
+    updateCompleteTotal = ()=> {
+        var total_complete = 0;
+        for (var i=0; i<this.state.list.length; i++) {
+            if ( this.state.list[i].status === 1 )
+                total_complete ++;
+        }
+        this.props.setCompleteTotal(total_complete);
     }
-}
 
-class Main extends React.Component {
+    componentDidMount(){
+        setInterval(this.clearCompleted, 500);
+        setInterval(this.updateTotal, 500);
+        setInterval(this.updateCompleteTotal, 500);
+    }
+
+    addTodo = (text)=> {
+        this.setState((state)=>({
+            id_count: state.id_count+1,
+            list: [new Todo(this.state.id_count, text), ...state.list]
+        }));
+        // console.log(this.state);
+    }
+
+    updateTodo = (id, todo)=> {
+        let list2 = [...this.state.list];
+        for (var i=0; i<list2.length; i++) {
+            if (list2[i].id === id) {
+                list2[i] = todo;
+                break;
+            }
+        }
+        // console.log(list2);
+        this.setState((state)=>({list: list2}));
+    }
+
+    delTodo = (id)=> {
+        let list2 = [...this.state.list];
+        for (var i=0; i<list2.length; i++)
+            if (list2[i].id === id) {
+                var idx = i;
+                break;
+            }
+        list2.splice(idx,1);
+        this.setState((state)=>({list: list2}));
+    }
+
+    getList = ()=> {
+        return this.state.list;
+    }
+
     render() {
         return (
             <section className="todo-app__main">
-                <Input todoList={todoList} todoItem={todoItem} />
-                <TodoList todoList={todoList} />
+                <Input addTodo={this.addTodo} />
+                <List getList={this.getList} 
+                      updateTodo={this.updateTodo} 
+                      delTodo={this.delTodo} 
+                      showMode={this.props.showMode}
+                />
             </section>
         );
     }
 }
 
-// TodoControl
+class List extends React.Component {
 
-class TodoTotal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {total: 0};
+    itemOnclick = (id)=> {
+        // console.log(id);
+        let list = this.props.getList();
+        for (var i=0; i<list.length; i++) {
+            if (list[i].id === id) {
+                var oldtodo = list[i];
+                break;
+            }
+        }
+        var text, new_status, new_checkStyle, new_checkLabelStyle;
+        if (oldtodo.status === 0){
+            text = oldtodo.text;
+            new_status = 1;
+            new_checkStyle = {"text-decoration": "line-through", "opacity": 0.5};
+            new_checkLabelStyle = {background: "green"};
+        } else {
+            text = oldtodo.text;
+            new_status = 0;
+            new_checkStyle = {};
+            new_checkLabelStyle = {};
+        }
+        var newtodo = new Todo(id, text, new_status, new_checkStyle, new_checkLabelStyle);
+        this.props.updateTodo(id, newtodo);
     }
 
-    componentDidMount = ()=>{
-        setInterval(
-            ()=>{
-                var total = 0;
-                for (var i=this.props.todoList.length-1; i>=0; i--) {
-                    var item = this.props.todoList[i];
-                    // console.log(item.status);
-                    if (!item.status) {
-                        total++;
-                    }
-                }
-                this.setState({total: total});
-            },
-            500);
+    render_rows = ()=> {        
+        var list = [];
+        var full_list = [...this.props.getList()];
+        for (var i=0; i<full_list.length; i++) {
+            if (this.props.showMode === 'all') {
+                list.push(full_list[i]);
+            } else if (this.props.showMode === 'active' && full_list[i].status === 0) {
+                list.push(full_list[i]);
+            } else if (this.props.showMode === 'completed' && full_list[i].status === 1) {
+                list.push(full_list[i]);
+            }
+        }
+
+        return list.map(e => 
+                <li key={e.id} className="todo-app__item" style={e.checkStyle}>
+                    <div className="todo-app__checkbox" onClick={()=>{this.itemOnclick(e.id)}}>
+                        <label id={e.id} style={e.checkLabelStyle} />
+                    </div>
+                    <h1 className="todo-app__item-detail">{e.text}</h1>
+                    <img src="./img/x.png" className="todo-app__item-x" onClick={()=>{this.props.delTodo(e.id);}}/>
+                </li>
+            );
     }
 
     render() {
         return (
-            <div className="todo-app__total">{this.state.total}</div>
+            <ul className="todo-app__list" id="todo-list">
+                { this.render_rows() }
+            </ul>
         );
     }
 }
 
-class TodoControl extends React.Component {
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value:''};
+    }
+
+    handleChange = (event)=>{
+        this.setState((state)=>({value: event.target.value}));
+    }
+
+    keyPress = (event)=>{
+        if (event.keyCode === 13) { //[enter] key
+            var text = this.state.value.trim();
+            if (text !== "") {
+                this.props.addTodo(text);
+            }
+            this.setState((state)=>({value:""}));
+        }
+    }
+
     render() {
         return (
-            <footer className="todo-app__footer" id="todo-footer">
-                <TodoTotal todoList={todoList} />
-                <ul className="todo-app__view-buttons">
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Complete</button>
-                </ul>
-                <div className="todo-app__clean">
-                    <button>Clear Complete</button>
-                </div>
-            </footer>
+            <input type="text"
+                className="todo-app__input"
+                value={this.state.value} 
+                onChange={this.handleChange} 
+                onKeyDown={this.keyPress} 
+            />
         );
     }
 }
--- hw3/own/src/index.js
+++ hw3/own/src/index.js
@@ -7,4 +7,3 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
-var todoList = [];
